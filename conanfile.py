fa700682d77de82dd808eb52f7c016abde9fefe9
Ulrich Eck <ulrich.eck@tum.de>
2018-11-19 18:34:47 +0100

    added missing file

--- conanfile.py
+++ conanfile.py
@@ -4,42 +4,39 @@ from conans import ConanFile, tools, CMake
 
 
 class rttrConan(ConanFile):
-    name = "rttr"
-    version = "0.9.7-dev"
+    name = "cppfs"
+    version = "1.2.0"
     license = "MIT License"
-    homepage = "https://www.github.com/rttrorg/rttr"
+    homepage = "https://www.github.com/cginternals/cppfs"
     description = """rttr project"""
-    url = "https://github.com/ulricheck/conan-rttr"
+    url = "https://github.com/ulricheck/conan-cppfs"
 
     generators = "cmake"
 
     settings = "os", "compiler", "build_type", "arch"
     options = {
         "shared": [True, False],
-        "rtti": [True, False],
         }
 
     default_options = (
         "shared=True", 
-        "rtti=True", 
     )
 
     scm = {
         "type": "git",
-        "subfolder": "rttr",
-        "url": "https://github.com/rttrorg/rttr.git",
+        "subfolder": "cppfs",
+        "url": "https://github.com/cginternals/cppfs.git",
         # "revision": "rttrorg-rttr-%s"% version
-        "revision": "master"
+        "revision": "v1.2"
      }
 
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["DBUILD_STATIC"] = not self.options.shared
-        cmake.definitions["BUILD_WITH_RTTI"] = self.options.rtti
-        cmake.configure(source_dir='rttr')
+        cmake.definitions["DBUILD_SHARED_LIBS"] = self.options.shared
+        cmake.configure(source_dir='cppfs')
         cmake.build()
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ["rttr_core_d"] if self.settings.build_type == "Debug" else ["rttr_core"]
+        self.cpp_info.libs = ["cppfs_d"] if self.settings.build_type == "Debug" else ["cppfs"]
