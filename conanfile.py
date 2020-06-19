6d149d9e229bf57d25db795c35c7fcef95d2a1a1
Ulrich Eck <ulrich.eck@tum.de>
2020-06-19 20:47:54 +0200

    bumped version number

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conans import ConanFile, tools, CMake
 
 class rttrConan(ConanFile):
     name = "cppfs"
-    version = "1.2.0"
+    version = "1.3.0"
     license = "MIT License"
     homepage = "https://www.github.com/cginternals/cppfs"
     description = """rttr project"""
@@ -27,16 +27,23 @@ class rttrConan(ConanFile):
         "subfolder": "cppfs",
         "url": "https://github.com/cginternals/cppfs.git",
         # "revision": "rttrorg-rttr-%s"% version
-        "revision": "v1.2"
+        "revision": "v%s" % version
      }
 
 
-    def build(self):
+    def _cmake_configure(self):
         cmake = CMake(self)
         cmake.definitions["DBUILD_SHARED_LIBS"] = self.options.shared
         cmake.configure(source_dir='cppfs')
+        return cmake
+
+    def build(self):
+        cmake = self._cmake_configure()
         cmake.build()
+
+    def package(self):
+        cmake = self._cmake_configure()
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ["cppfs_d"] if self.settings.build_type == "Debug" else ["cppfs"]
+        self.cpp_info.libs = tools.collect_libs(self)
